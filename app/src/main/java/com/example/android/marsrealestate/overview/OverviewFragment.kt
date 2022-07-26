b611c7ba39996a623649bd35bc2c8256e9a5d1a8
Katarzyna Bedkowska <kbedkowska@C12339>
2022-07-26 14:12:05 +0200

    first commit

---
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -0,0 +1,33 @@
+package com.example.android.marsrealestate.overview
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.marsrealestate.R
+import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
+import com.example.android.marsrealestate.databinding.GridViewItemBinding
+
+class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
+
+        setHasOptionsMenu(true)
+        binding.photosGrid.adapter = PhotoGridAdapter()
+
+        return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+}
